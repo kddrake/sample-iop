ac0637114ee01174177ecaea0b54e519089aa0e2
Kean Drake <kean.drake@banno.com>
2015-08-01 16:29:13 -0500

    Created connect and reload tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,14 @@
 var gulp = require('gulp');
+var connect = require('gulp-connect');
+
+gulp.task('connect', function(){
+  connect.server({
+    port: 1820,
+    livereload: true
+  });
+});
+
+gulp.task('reload:src', function(){
+  gulp.src('./src')
+  .pipe(connect.reload());
+});
