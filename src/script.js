1f9e92c5a9a71394860e12372e56a5f319c60c6f
Kean Drake <kean.drake@banno.com>
2015-08-03 14:58:06 -0500

    Added expanding and minimizing functionality

---
+++ src/script.js
@@ -0,0 +1,9 @@
+function minimize() {
+  document.getElementById("userPhone").style.display = 'none';
+  document.getElementById("userEmail").style.display = 'none';
+};
+
+function expand() {
+  document.getElementById("userPhone").style.display = 'block';
+  document.getElementById("userEmail").style.display = 'block';
+};
