2838ea9c0a868ca737e0222e96ae63081b29ea82
Kean Drake <kean.drake@banno.com>
2015-08-05 10:52:31 -0500

    Added gulp-help and jshint-stylish. Attempted to add Karma

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,29 @@
-var gulp = require('gulp');
+var gulp = require('gulp-help')(require('gulp'));
 var sass = require('gulp-sass')
 var jshint = require('gulp-jshint');
-var stylish = require('jshint-stylish')
+var stylish = require('jshint-stylish');
+var Server = require('karma').Server;
 var connect = require('gulp-connect');
 
-gulp.task('sass', function() {
-  gulp.src('src/*.scss')
+gulp.task('sass', 'Compiles preprocessed SASS files to a CSS file', function() {
+  gulp.src('src/sass/*.scss')
   .pipe(sass().on('error', sass.logError))
   .pipe(gulp.dest('src'))
 });
 
-gulp.task('jshint', function() {
-  return gulp.src('src/*.js')
+gulp.task('jshint', 'Detects errors and potential problems in JavaScript', function() {
+  return gulp.src('src/js/*.js')
   .pipe(jshint())
   .pipe(jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('connect', function(){
+gulp.task('unit-test', 'Runs Karma unit tests', function(done){
+  new Server({
+    configFile: './test/unit/karma.conf.js'
+  }, done).start();
+});
+
+gulp.task('connect', 'Webserver with LiveReload at localhost:1820 serving up \'src/\'', function(){
   connect.server({
     root: 'src',
     port: 1820,
@@ -24,15 +31,15 @@ gulp.task('connect', function(){
   });
 });
 
-gulp.task('reload:src', function(){
+gulp.task('reload:src', false, function(){
   gulp.src('./src')
   .pipe(connect.reload());
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', 'Watches files and runs corresponding gulp tasks', function(){
   gulp.watch(['src/*.html', 'src/*.css'], ['reload:src']);
   gulp.watch(['src/*.scss'], ['sass']);
   gulp.watch(['src/*.js'], ['jshint']);
 });
 
-gulp.task('default', ['sass', 'jshint', 'connect', 'watch']);
+gulp.task('default', false, ['sass', 'jshint', 'unit-test', 'connect', 'watch']);
